4a4c6b7029cf6d06a6d59ec02a51457296f3c15d
aleksandar1021 <93982039+aleksandar1021@users.noreply.github.com>
2021-12-09 19:58:50 +0100

    Delete JavaScript1.js

--- JavaScript1.js
+++
@@ -1,7 +0,0 @@
-﻿function fun(x, y) {
-    document.getElementById("opisslike").innerHTML = y;
-    var z = document.getElementsByClassName("slikaa");
-    document.getElementById("pozornicaslika").src = z[x].src;
-}
-
-
