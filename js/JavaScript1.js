f654f464dcd77095f15c249ef06c8b874fe92701
aleksandar1021 <93982039+aleksandar1021@users.noreply.github.com>
2021-12-09 20:02:01 +0100

    Add files via upload

---
+++ js/JavaScript1.js
@@ -0,0 +1,7 @@
+﻿function fun(x, y) {
+    document.getElementById("opisslike").innerHTML = y;
+    var z = document.getElementsByClassName("slikaa");
+    document.getElementById("pozornicaslika").src = z[x].src;
+}
+
+
